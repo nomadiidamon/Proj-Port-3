e317a47b5b16c3391042084619fe1ec0c43b33d8
Johnny Paschen <johnpaschen@gmail.com>
2024-09-19 14:44:11 -0700

    lava script started

---
+++ Platforming-FPS/Assets/Scripts/Johnny/Lava.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lava : MonoBehaviour, IDamage
+{
+    [SerializeField] private int lavaDamage;
+    [SerializeField] private float dmgDelay;
+
+    private bool isPool;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void takeDamage(int amt)
+    {
+        
+    }
+}
