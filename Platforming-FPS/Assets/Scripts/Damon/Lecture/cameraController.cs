078a0d28af24ca1ab14cc9e7e0ad3217ccdb082b
Johnny Paschen <johnpaschen@gmail.com>
2024-09-22 09:36:33 -0700

    fixed camera collision

--- Platforming-FPS/Assets/Scripts/Damon/Lecture/cameraController.cs
+++ Platforming-FPS/Assets/Scripts/Damon/Lecture/cameraController.cs
@@ -11,16 +11,21 @@ public class cameraController : MonoBehaviour
     public bool invertY;
 
     public Camera playerCamera;
+    public Transform playerTransform;
     public float minFOV = 30f;
     public float maxFOV = 300f;
+    public float cameraDistance = 0f;
+    public LayerMask collisionMask;
 
     float rotX;
+    Vector3 cameraOffset;
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+        cameraOffset = playerCamera.transform.localPosition.normalized * cameraDistance;
     }
 
     // Update is called once per frame
@@ -44,6 +49,27 @@ public class cameraController : MonoBehaviour
         //rotate the PLAYER on the y-axis
         transform.parent.Rotate(Vector3.up * mouseX);
 
+        // camera collison
+        HandleCameraCollision();
+
+    }
+
+    void HandleCameraCollision()
+    {
+        Vector3 newPos = playerTransform.position - transform.forward * cameraDistance;
+        RaycastHit hit;
+
+        // Perform a raycast to check if there is an object between the player and the camera
+        if (Physics.Raycast(playerTransform.position, -transform.forward, out hit, cameraDistance, collisionMask))
+        {
+            // Move the camera closer if there's a collision
+            playerCamera.transform.position = hit.point;
+        }
+        else
+        {
+            // No collision, set the camera to its desired position
+            playerCamera.transform.position = newPos;
+        }
     }
 
     public void setSens(float newSens)
