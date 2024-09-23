9c68a04efb4bc72f9a386f89c20e16cdd43c9466
DMDanser <155984953+DMDanser@users.noreply.github.com>
2024-09-22 20:57:01 -0400

    Created MovineObject script
    float horizontalMove
    float verticalMove
    float forwardMove
    
    float moveSpeed
    
    Thought about adding isHazard bool, but probably just as easy to drop a Damage script on the object

---
+++ Platforming-FPS/Assets/Scripts/Dylan/MovingObject.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    [SerializeField] float horizontalMove;
+    [SerializeField] float verticalMove;
+    [SerializeField] float forwardMove;
+    [Range(0.1f, 3f)][SerializeField] float moveSpeed;
+
+    Vector3 startingPosition;
+
+    float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+    private void Update()
+    {
+        timer += Time.deltaTime * moveSpeed;
+        float newX = Mathf.Cos(timer) * horizontalMove;
+        float newY = Mathf.Sin(timer) * verticalMove;
+        float newZ = Mathf.Cos(timer) * forwardMove;
+
+        transform.position = startingPosition + new Vector3(newX, newY, newZ);
+    }
+}
